2a99c2d5b55a570bf9e89a6d960102444d86d495
Nikolas Petrou <36922092+nikopetr@users.noreply.github.com>
2021-01-27 17:28:05 +0200

    Fixed indexer, changed top-k feedback

--- my_indexer_threading.py
+++ my_indexer_threading.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-import collections
 import math
 import threading
 import time
@@ -28,7 +27,7 @@ class InvertedIndexer(threading.Thread):
     # Locks for global(static) variables
     lock_indexer = threading.Lock()
     lock_documents_data = threading.Lock()
-    lock_count = threading.Lock()
+    lock_queue = threading.Lock()
 
     # Initialising indexer and it's static variables for the indexer
     @staticmethod
@@ -41,10 +40,21 @@ class InvertedIndexer(threading.Thread):
         if not new_indexer:
             InvertedIndexer.indexer = np.load(InvertedIndexer.INVERTED_INDEXER_FILENAME, allow_pickle=True).item()
             InvertedIndexer.documents_metadata = np.load(InvertedIndexer.DOC_METADATA_FILENAME, allow_pickle=True).item()
+        else:
+            for k, v in InvertedIndexer.document_dict.items():
+                pair = (k, v)
+                InvertedIndexer.document_queue.put(pair)
 
-        for k, v in InvertedIndexer.document_dict.items():
-            pair = (k, v)
-            InvertedIndexer.document_queue.put(pair)
+    @staticmethod
+    def get_frequency_dict(term_list):
+        # Creating an empty dictionary
+        freq_dict = {}
+        for item in term_list:
+            if item in freq_dict:
+                freq_dict[item] += 1
+            else:
+                freq_dict[item] = 1
+        return freq_dict
 
     def __init__(self):
         threading.Thread.__init__(self)
@@ -52,74 +62,67 @@ class InvertedIndexer(threading.Thread):
 
     # Method that creates the indexer
     def run(self):
-        # While loop because we want to run this function multiple times
+        print(threading.current_thread().__str__() + "has started" + '\n')
+        # Thread sleep-delay before each of the thread's FIRST start only
+        time.sleep(0.1)
+
+        # Running until all documents are checked
         while True:
-            InvertedIndexer.lock_count.acquire()
-            if InvertedIndexer.document_queue.empty() is False:
-
-                doc, (title, words) = InvertedIndexer.document_queue.get()
-                InvertedIndexer.lock_count.release()
-
-                # Checks to find only unvisited documents
-                # (in the case of running the algorithm for another time after the inverted dictionary was already made)
-                InvertedIndexer.lock_documents_data.acquire()
-                if doc in InvertedIndexer.documents_metadata.keys():
-                    InvertedIndexer.lock_documents_data.release()
-                    continue
-                InvertedIndexer.lock_documents_data.release()
-
-                self.docs_processed += 1
-                InvertedIndexer.lock_indexer.acquire()
-                for pos, word in enumerate(words):
-                    # word doesn't exist add it in indexer { word : (1,(doc,position)) }
-                    # InvertedIndexer.lock_indexer.acquire()
-                    if word not in InvertedIndexer.indexer:
-                        InvertedIndexer.indexer[word] = (1, (doc, pos))
-                        # InvertedIndexer.lock_indexer.release()
-                        # If the word exists, then update record like this {word : ((n+1),...docs...,(doc,position)}
-                    else:
-                        # Get record for the word
-                        values = InvertedIndexer.indexer.get(word)
-                        # InvertedIndexer.lock_indexer.release()
-                        values_l = list(values)  # Transform tuple to list in order to change it
-                        # If the word was already found in the document doc
-                        doc_l = [x[0] for x in values_l[1:]]
-                        if doc in doc_l:
-                            temp = doc_l.index(doc)
-                            # We use temp+1 because we search from 1....n-1 elements of the list
-                            positions_l = list(values_l[temp+1])
-                            positions_l.append(pos)
-                            positions = tuple(positions_l)
-                            values_l[temp+1] = positions
-                            # If this is the first appearance of word in doc
-                        else:
-                            n = values[0]
-                            values_l[0] = n+1
-                            values_l.append((doc, pos))
-                        values = tuple(values_l)
-                        # InvertedIndexer.lock_indexer.acquire()
-                        InvertedIndexer.indexer.update({word: values})  # Update indexer
-                        # InvertedIndexer.lock_indexer.release()
-                InvertedIndexer.lock_indexer.release()
-                # Find the max_freq and Ld of document
-                freq = collections.Counter(words)
-
-                if len(freq.values()) != 0:
-                    max_freq = max(freq.values())
-                    #  The length of the vectors of the document
-                    ld = math.sqrt(sum([x**2 for x in freq.values()])) / max_freq
-                else:
-                    max_freq = 0
-                    ld = 0
-
-                InvertedIndexer.lock_documents_data.acquire()
-                InvertedIndexer.documents_metadata[doc] = (ld, max_freq, title)
-                InvertedIndexer.lock_documents_data.release()
-            else:
-                InvertedIndexer.lock_count.release()
+            InvertedIndexer.lock_queue.acquire()
+            if InvertedIndexer.document_queue.empty() is True:
+                InvertedIndexer.lock_queue.release()
                 break
 
-        print(threading.current_thread().__str__() + "has finished. Total docs processed: " + self.docs_processed.__str__())
+            else:
+                doc, (title, words) = InvertedIndexer.document_queue.get()
+                InvertedIndexer.lock_queue.release()
+
+            # Checks to find only unvisited documents
+            # (in the case of running the algorithm for another time after the inverted dictionary was already made)
+            InvertedIndexer.lock_documents_data.acquire()
+            if doc in InvertedIndexer.documents_metadata.keys():
+                InvertedIndexer.lock_documents_data.release()
+                continue
+            InvertedIndexer.lock_documents_data.release()
+
+            self.docs_processed += 1
+
+            word_freq_dict = InvertedIndexer.get_frequency_dict(words)
+
+            for word, word_count in word_freq_dict.items():
+                InvertedIndexer.lock_indexer.acquire()
+                # Word doesn't exist add it in the indexer { word : [1,[(doc,word_count)]]}
+                if word not in InvertedIndexer.indexer:
+                    # Update indexer with the new word
+                    InvertedIndexer.indexer[word] = [1, [(doc, word_count)]]
+                else:
+                    # Get record for the word
+                    values = InvertedIndexer.indexer.get(word)
+                    InvertedIndexer.lock_indexer.release()
+                    # Increase appearances in docs
+                    values[0] += 1
+                    # Append the document with it's word count
+                    values[1].append((doc, word_count))
+                    # Update indexer
+                    InvertedIndexer.lock_indexer.acquire()
+                    InvertedIndexer.indexer.update({word: values})  # Update indexer
+                InvertedIndexer.lock_indexer.release()
+
+            # Find the max_freq and Ld of document
+            if len(word_freq_dict.values()) != 0:
+                max_freq = max(word_freq_dict.values())
+                #  The length of the vectors of the document
+                ld = math.sqrt(sum([x ** 2 for x in word_freq_dict.values()])) / max_freq
+            else:
+                max_freq = 0
+                ld = 0
+
+            InvertedIndexer.lock_documents_data.acquire()
+            InvertedIndexer.documents_metadata[doc] = (ld, max_freq, title)
+            InvertedIndexer.lock_documents_data.release()
+
+        print(threading.current_thread().__str__() + "has finished. Total docs processed: "
+              + self.docs_processed.__str__())
 
 
 # Updates the indexer in case of the desire to add any new data contained in data.npy
@@ -138,6 +141,7 @@ def update_indexer(number_of_threads=1):
     # print(InvertedIndexer.documents_metadata)
     print("Time needed for indexer update: %f seconds" % (index_end_time-index_start_time))
 
+    # todo change where this is
     # Saving Inverted Indexer and documents' meta-data
     np.save(InvertedIndexer.INVERTED_INDEXER_FILENAME, InvertedIndexer.indexer)
     np.save(InvertedIndexer.DOC_METADATA_FILENAME, InvertedIndexer.documents_metadata)
@@ -145,4 +149,4 @@ def update_indexer(number_of_threads=1):
 
 # IN ORDER TO RUN ALONE TODO DELETE THIS
 # InvertedIndexer.init_indexer(True)
-# update_indexer(2)
+# update_indexer(1)
--- my_query_processor.py
+++ my_query_processor.py
@@ -17,6 +17,10 @@ def idf(total_docs, docs_with_term):
 
 class QueryProcessor:
 
+    # Different weights constants for positive and negative feedback
+    POS_FEEDBACK_W = 0.5
+    NEG_FEEDBACK_W = -1 * 0.25
+
     # indexer_update_threads parameter has no effect if parameter update_indexer_from_datafile is False
     def __init__(self, new_indexer=True, update_indexer_from_datafile=True, indexer_update_threads=1):
         my_indexer_threading.InvertedIndexer.init_indexer(new_indexer)
@@ -28,36 +32,68 @@ class QueryProcessor:
             my_indexer_threading.update_indexer(number_of_threads=indexer_update_threads)
 
     def update_accumulator_for_term(self, term, tf_tq):
+        # The number of total documents
+        total_docs = len(my_indexer_threading.InvertedIndexer.documents_metadata)
+
         # Searching term in the indexer
         term_indexer = my_indexer_threading.InvertedIndexer.indexer.get(term)
 
+        # Structure of term_indexer: { term : [n_t, [(doc_0, term_count_in_doc_0), ... , (doc_n, term_count_in_doc_n)]]}
+
         # If the term exists in the indexer (in any documents)
         if term_indexer is not None:
+
             # The number of docs that contain the term
             n_t = term_indexer[0]
-            total_docs = len(my_indexer_threading.InvertedIndexer.documents_metadata)
+
+            # Calculate idf for the term
             idf_t = idf(total_docs, n_t)
 
-            doc_indexer = term_indexer[1:]
-            # Getting rid of the first item (which is the number of documents)
-            for d_indexes in doc_indexer:
-                # This part is the name/url of the document
-                document = d_indexes[0]
+            # This is the array with the items that have the document's url with
+            # the frequency of the term in the current document
+            doc_indexer = term_indexer[1]
 
-                # The part which is the frequency list of that term in the current document
-                freq_list = d_indexes[1:]
+            # For all the documents that contain this term/word,
+            # Take name/url of the document
+            # and the frequency of the term in the current document
+            for document, freq_td in doc_indexer:
 
-                freq_td = len(freq_list)  # The frequency of the term in the current document
+                # The max_frequency of among all the terms in that document
                 max_freq_doc = my_indexer_threading.InvertedIndexer.documents_metadata.get(document)[1]
+
+                # Calculating tf for the term
                 tf_td = tf(freq_td, max_freq_doc)
 
-                # Updating accumulator
+                # Updating accumulator with the updated score of the tf-idf weight
                 doc_score = self.accumulators.get(document)
                 if doc_score is not None:
                     updated_score = doc_score + (tf_td * idf_t) * tf_tq
                     self.accumulators[document] = updated_score
                 else:
                     self.accumulators[document] = (tf_td * idf_t) * tf_tq
+
+    # Update the accumulators in order to keep only the top k documents,
+    # return the top-k of the documents with their titles
+    def get_top_k_documents_title_dict(self, k=1):
+
+        # The top k largest keys in the dictionary of accumulators
+        # Using heapq in order to return the top k documents in an efficient time of O(n log k)
+        top_k_keys = heapq.nlargest(k, self.accumulators, key=self.accumulators.get)
+
+        top_k_accumulator = {}
+        top_k_documents_title_dict = {}
+
+        # Adding the top_k documents with their similarities in a new dictionary,
+        # also adding the top_k documents with their titles in a another dictionary that will be returned
+        for document in top_k_keys:
+            top_k_accumulator.update({document: self.accumulators.get(document)})
+            doc_title = my_indexer_threading.InvertedIndexer.documents_metadata.get(document)[2]
+            top_k_documents_title_dict.update({document: doc_title})
+
+        # Update the accumulator to have only the top k documents
+        self.accumulators = top_k_accumulator
+
+        return top_k_documents_title_dict
 
     def top_k(self, query, k=1):
         self.query_vector = {}
@@ -67,7 +103,6 @@ class QueryProcessor:
 
         # Applying the same pre-processing on the queries that was used in the crawler
         query_terms = my_text_processor.TextProcessor.get_useful_word_list(query, stemming=True)
-
         # The frequencies of the terms in the query
         terms_frequencies_dict = collections.Counter(query_terms)
         #  The frequency of the most frequent term in the query
@@ -85,21 +120,15 @@ class QueryProcessor:
             # Update the documents' accumulator for that term
             self.update_accumulator_for_term(term, tf_tq)
 
-        returned_documents = {}
-
         # Updating similarities of accumulators based on the lengths
         for document in self.accumulators.keys():
             ld = my_indexer_threading.InvertedIndexer.documents_metadata.get(document)[0]
-            title = my_indexer_threading.InvertedIndexer.documents_metadata.get(document)[2]
             similarity = self.accumulators.get(document)/(ld*lq)
             self.accumulators.update({document: similarity})
-            returned_documents.update({document: title})
-        print(self.accumulators)
 
-        # Returning top k largest keys in the dictionary
-        # Using heapq in order to return the top k documents in an efficient time of O(n log k)
-        top_k_keys = heapq.nlargest(k, self.accumulators, key=self.accumulators.get)
-        return {k: returned_documents[k] for k in top_k_keys}
+        # Update the accumulators in order to keep only the top k documents,
+        # return the top-k of the documents with their titles
+        return self.get_top_k_documents_title_dict(k)
 
     def top_k_feedback(self, k=1):
         # Initialise Accumulators
@@ -110,76 +139,153 @@ class QueryProcessor:
             tf_tq = self.query_vector.get(term)
             self.update_accumulator_for_term(term, tf_tq)
 
-        # List for irrelevant keys (documents) in order to delete them from the output
-        irrelevant_keys = []
-
-        returned_documents = {}
-
         # Updating similarities of accumulators based on the lengths
         for document in self.accumulators.keys():
             ld = my_indexer_threading.InvertedIndexer.documents_metadata.get(document)[0]
             similarity = self.accumulators.get(document)/ld
+            self.accumulators.update({document: similarity})
 
-            # If the similarity shows that the specific document has a negative similarity value,
-            # then mark it as irrelevant by adding the key to the irrelevant_keys list
-            if similarity >= 0:
-                self.accumulators.update({document: similarity})
-                title = my_indexer_threading.InvertedIndexer.documents_metadata.get(document)[2]
-                returned_documents.update({document: title})
-            else:
-                irrelevant_keys.append(document)
+        # Update the accumulators in order to keep only the top k documents,
+        # return the top-k of the documents with their titles
+        return self.get_top_k_documents_title_dict(k)
 
-        # Delete irrelevant keys (documents) fom the accumulators
-        for document in irrelevant_keys:
-            del self.accumulators[document]
-        print(self.accumulators)
-        # Returning top k largest keys in the dictionary
-        # Using heapq in order to return the top k documents in an efficient time of O(n log k)
-        top_k_keys = heapq.nlargest(k, self.accumulators, key=self.accumulators.get)
-        return {k: returned_documents[k] for k in top_k_keys}
-
-    def feedback(self, feedback_docs,k):
+    def feedback(self, feedback_docs, k):
+        # import time
+        # start = time.time()
         relevant_docs = set(feedback_docs)
         non_relevant_docs = set()
         for document in self.accumulators.keys():
             if document not in relevant_docs:
                 non_relevant_docs.add(document)
 
-        indexer = my_indexer_threading.InvertedIndexer.indexer
         meta_data_docs = my_indexer_threading.InvertedIndexer.documents_metadata
+
+        # The number of total documents
+        total_docs = len(meta_data_docs)
+
+        # The inverted indexer with the terms as keys
+        indexer = my_indexer_threading.InvertedIndexer.indexer
+
         for term in indexer:
-            values = indexer.get(term)
+            term_indexer = indexer.get(term)
 
             # The number of docs that contain the term
-            n_t = values[0]
-            # The total docs
-            total_docs = len(meta_data_docs)
+            n_t = term_indexer[0]
+
             # Calculate idf for the term
             idf_t = idf(total_docs, n_t)
-            # For every document that contains the term
-            for i in range(1, len(values)):
-                doc = values[i][0]
 
-                if doc in relevant_docs:
-                    freq_td = len(values)-1  # The frequency of the term in the current document
-                    max_freq_doc = meta_data_docs.get(doc)[1]
+            # This is the array with the items that have the document's url with
+            # the frequency of the term in the current document
+            doc_indexer = term_indexer[1]
+
+            # For every document that contains the term make changes to the "query vector"
+            for document, freq_td in doc_indexer:
+
+                set_of_document = None
+                weight = None
+
+                if document in relevant_docs:
+                    set_of_document = relevant_docs
+                    # Since the document had positive feedback multiply with positive weight
+                    weight = QueryProcessor.POS_FEEDBACK_W
+
+                elif document in non_relevant_docs:
+                    set_of_document = non_relevant_docs
+                    # Since the document had negative feedback multiply with negative weight
+                    weight = QueryProcessor.NEG_FEEDBACK_W
+
+                if set_of_document is not None:
+                    max_freq_doc = meta_data_docs.get(document)[1]
                     tf_td = tf(freq_td, max_freq_doc)
 
                     # If the term already exists in the keys of the dictionary then take the previous value,
                     # else start from 0
-                    prev_total = self.query_vector.get(term) if self.query_vector.get(term) is not None else 0
-                    self.query_vector.update({term: prev_total + (0.5 * (idf_t * tf_td))/len(relevant_docs)})
+                    prev_total = self.query_vector.get(term)
+                    if prev_total is None:
+                        prev_total = 0
 
-                elif doc in non_relevant_docs:
-                    freq_td = len(values)-1  # The frequency of the term in the current document
-                    max_freq_doc = meta_data_docs.get(doc)[1]
-                    tf_td = tf(freq_td, max_freq_doc)
+                    # Update the query vector
+                    self.query_vector.update({term: prev_total + (weight * (idf_t * tf_td))/len(set_of_document)})
 
-                    # If the term already exists in the keys of the dictionary then take the previous value,
-                    # else start from 0
-                    prev_total = self.query_vector.get(term) if self.query_vector.get(term) is not None else 0
-                    self.query_vector.update({term: prev_total - (0.25 * (idf_t * tf_td))/len(non_relevant_docs)})
-        # print(self.query_vector)
+        # print("Total time for feedback: ", (time.time() - start))
         return self.top_k_feedback(k)
+
+    # @staticmethod
+    # def find_terms_in_feedback_docs(feedback_docs):
+    #     terms = set()
+    #
+    #     for document in feedback_docs:
+    #         (_title, doc_terms) = my_indexer_threading.InvertedIndexer.document_dict.get(document)
+    #
+    #         for term in doc_terms:
+    #             terms.add(term)
+    #     return terms
+
+    # def feedback_new(self, positive_feedback_docs, k):
+    #     import time
+    #     start = time.time()
+    #     relevant_docs = set(positive_feedback_docs)
+    #     non_relevant_docs = set()
+    #     for document in self.accumulators.keys():
+    #         if document not in relevant_docs:
+    #             non_relevant_docs.add(document)
+    #
+    #     meta_data_docs = my_indexer_threading.InvertedIndexer.documents_metadata
+    #
+    #     # The number of total documents
+    #     total_docs = len(meta_data_docs)
+    #
+    #     print(self.accumulators.keys())  # TODO delete ths line
+    #
+    #     feedback_terms = QueryProcessor.find_terms_in_feedback_docs(self.accumulators.keys())
+    #
+    #     # The inverted indexer with the terms as keys
+    #     indexer = my_indexer_threading.InvertedIndexer.indexer
+    #
+    #     for term in feedback_terms:
+    #         term_indexer = indexer.get(term)
+    #
+    #         # The number of docs that contain the term
+    #         n_t = term_indexer[0]
+    #
+    #         # Calculate idf for the term
+    #         idf_t = idf(total_docs, n_t)
+    #
+    #         # This is the array with the items that have the document's url with
+    #         # the frequency of the term in the current document
+    #         doc_indexer = term_indexer[1]
+    #
+    #         # For every document that contains the term make changes to the "query vector"
+    #         for document, freq_td in doc_indexer:
+    #
+    #             set_of_document = None
+    #             weight = None
+    #
+    #             if document in relevant_docs:
+    #                 set_of_document = relevant_docs
+    #                 # Since the document had positive feedback multiply with positive weight
+    #                 weight = QueryProcessor.POS_FEEDBACK_W
+    #
+    #             elif document in non_relevant_docs:
+    #                 set_of_document = non_relevant_docs
+    #                 # Since the document had negative feedback multiply with negative weight
+    #                 weight = QueryProcessor.NEG_FEEDBACK_W
+    #
+    #             if set_of_document is not None:
+    #                 max_freq_doc = meta_data_docs.get(document)[1]
+    #                 tf_td = tf(freq_td, max_freq_doc)
+    #
+    #                 # If the term already exists in the keys of the dictionary then take the previous value,
+    #                 # else start from 0
+    #                 prev_total = self.query_vector.get(term)
+    #                 if prev_total is None:
+    #                     prev_total = 0
+    #
+    #                 # Update the query vector
+    #                 self.query_vector.update({term: prev_total + (weight * (idf_t * tf_td))/len(set_of_document)})
+    #
+    #     print("feed back new time: ", (time.time() - start))
+    #     return self.top_k_feedback(k)
 
 
